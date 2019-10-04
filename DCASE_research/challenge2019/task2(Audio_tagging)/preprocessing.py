2b66e3ff92c46afea658873d6807a56d4796d645
Nikita <paniquexx@gmail.com>
2019-10-04 16:31:52 +0300

    add basic functions for read audio and getting spectrograms

--- DCASE_research/challenge2019/task2(Audio_tagging)/preprocessing.py
+++ DCASE_research/challenge2019/task2(Audio_tagging)/preprocessing.py
@@ -4,3 +4,58 @@
 #  2) Filterbanks
 #  3) Log spectrograms
 #  4) ...
+import librosa
+import librosa.display
+import IPython
+import IPython.display
+import matplotlib.pyplot as plt  # for debug in jupyter-notebook
+import numpy as np
+
+
+def read_audio(conf, pathname, trim_long_data):
+    y, sr = librosa.load(pathname, sr=conf.sampling_rate)
+    # trim silence
+    if 0 < len(y): # workaround: 0 length causes error
+        y, _ = librosa.effects.trim(y) # trim, top_db=default(60)
+    # make it unified length to conf.samples
+    if len(y) > conf.samples: # long enough
+        if trim_long_data:
+            y = y[0:0+conf.samples]
+    else: # pad blank
+        padding = conf.samples - len(y)    # add padding at both ends
+        offset = padding // 2
+        y = np.pad(y, (offset, conf.samples - len(y) - offset), conf.padmode)
+    return y
+
+
+def audio_to_melspectrogram(conf, audio):
+    spectrogram = librosa.feature.melspectrogram(audio,
+                                                 sr=conf.sampling_rate,
+                                                 n_mels=conf.n_mels,
+                                                 hop_length=conf.hop_length,
+                                                 n_fft=conf.n_fft+10,
+                                                 fmin=conf.fmin,
+                                                 fmax=conf.fmax,
+                                                 window=conf.window_type)
+    spectrogram = librosa.power_to_db(spectrogram)
+    spectrogram = spectrogram.astype(np.float32)
+    return spectrogram
+
+
+# For debug in jupyter-notebook
+def show_melspectrogram(conf, mels, title='Log-frequency power spectrogram'):
+    librosa.display.specshow(mels, x_axis='time', y_axis='mel',
+                             sr=conf.sampling_rate, hop_length=conf.hop_length,
+                            fmin=conf.fmin, fmax=conf.fmax)
+    plt.colorbar(format='%+2.0f dB')
+    plt.title(title)
+    plt.show()
+
+
+def read_as_melspectrogram(conf, pathname, trim_long_data, debug_display=False):
+    x = read_audio(conf, pathname, trim_long_data)
+    mels = audio_to_melspectrogram(conf, x)
+    if debug_display:
+        IPython.display.display(IPython.display.Audio(x, rate=conf.sampling_rate))
+        show_melspectrogram(conf, mels)
+    return mels
